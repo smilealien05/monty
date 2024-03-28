0bd156dfcae958035ff91c10d6eb943ba542d94e
smilealien05 <ismailstarallah0@gmail.com>
2024-03-28 15:10:59 -0700

    $ gcc -Wall -Werror -Wextra -pedantic -std=c89 *.c -o monty

---
+++ func_help.c
@@ -0,0 +1,87 @@
+#include "monty.h"
+
+
+/**
+ * add_to_stack - Adds a node to the stack.
+ * @new_node: Pointer to the new node.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *ttmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	ttmp = head;
+	head = *new_node;
+	head->next = ttmp;
+	ttmp->prev = head;
+}
+
+
+/**
+ * print_stack - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: line number of  the opcode.
+ */
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ttmp;
+
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	ttmp = *stack;
+	while (ttmp != NULL)
+	{
+		printf("%d\n", ttmp->n);
+		ttmp = ttmp->next;
+	}
+}
+
+/**
+ * pop_top - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void pop_top(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ttmp;
+
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
+
+	ttmp = *stack;
+	*stack = ttmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(ttmp);
+}
+
+/**
+ * print_top - Prints the top node of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void print_top(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_number);
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
